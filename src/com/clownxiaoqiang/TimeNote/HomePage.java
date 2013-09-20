dba3be602e270987cd409c95ca80f8aa0251ac46
1019439568 <www.dzq18672103755@sina.cn>
2013-09-20 21:48:42 +0800

    TimeNote_xq

---
+++ src/com/clownxiaoqiang/TimeNote/HomePage.java
@@ -0,0 +1,36 @@
+package com.clownxiaoqiang.TimeNote;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: z
+ * Date: 13-9-20
+ * Time: afternoon 7:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomePage extends Activity {
+
+    private int Screen_x,Screen_y;
+    private DrawCircle drawCircle;
+    private RelativeLayout relativeLayout;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(R.layout.homepage);
+        DisplayMetrics displayMetrics =new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        Screen_x = displayMetrics.widthPixels;
+        Screen_y = displayMetrics.heightPixels;
+        //drawCircle = (DrawCircle)findViewById(R.id.drawcirlce);
+       // drawCircle =new DrawCircle(this,Screen_x,Screen_y);
+        drawCircle =new DrawCircle(this,Screen_x,Screen_y);
+        setContentView(drawCircle);
+    }
+}
